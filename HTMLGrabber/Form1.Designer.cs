d24b526f123f91bb2507fc50fba8853613dfe761
Thomas Sapp <tpsapp@hotmail.com>
2020-11-12 19:06:45 -0700

    Add project files.

---
+++ HTMLGrabber/Form1.Designer.cs
@@ -0,0 +1,155 @@
+﻿namespace SocketTest
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.dataOuput = new System.Windows.Forms.RichTextBox();
+            this.flowLayoutPanel1 = new System.Windows.Forms.FlowLayoutPanel();
+            this.exitButton = new System.Windows.Forms.Button();
+            this.getPage = new System.Windows.Forms.Button();
+            this.urlInput = new System.Windows.Forms.TextBox();
+            this.tableLayoutPanel1.SuspendLayout();
+            this.flowLayoutPanel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 2;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 60F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel1.Controls.Add(this.label1, 0, 0);
+            this.tableLayoutPanel1.Controls.Add(this.dataOuput, 1, 0);
+            this.tableLayoutPanel1.Controls.Add(this.flowLayoutPanel1, 0, 1);
+            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 2;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 35F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(532, 505);
+            this.tableLayoutPanel1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(3, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(53, 26);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Received Data:";
+            // 
+            // dataOuput
+            // 
+            this.dataOuput.BackColor = System.Drawing.Color.White;
+            this.dataOuput.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.dataOuput.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.dataOuput.Location = new System.Drawing.Point(63, 3);
+            this.dataOuput.Name = "dataOuput";
+            this.dataOuput.ReadOnly = true;
+            this.dataOuput.Size = new System.Drawing.Size(466, 464);
+            this.dataOuput.TabIndex = 4;
+            this.dataOuput.Text = "";
+            // 
+            // flowLayoutPanel1
+            // 
+            this.tableLayoutPanel1.SetColumnSpan(this.flowLayoutPanel1, 2);
+            this.flowLayoutPanel1.Controls.Add(this.exitButton);
+            this.flowLayoutPanel1.Controls.Add(this.getPage);
+            this.flowLayoutPanel1.Controls.Add(this.urlInput);
+            this.flowLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.flowLayoutPanel1.FlowDirection = System.Windows.Forms.FlowDirection.RightToLeft;
+            this.flowLayoutPanel1.Location = new System.Drawing.Point(3, 473);
+            this.flowLayoutPanel1.Name = "flowLayoutPanel1";
+            this.flowLayoutPanel1.Size = new System.Drawing.Size(526, 29);
+            this.flowLayoutPanel1.TabIndex = 2;
+            this.flowLayoutPanel1.Resize += new System.EventHandler(this.flowLayoutPanel1_Resize);
+            // 
+            // exitButton
+            // 
+            this.exitButton.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.exitButton.Location = new System.Drawing.Point(448, 3);
+            this.exitButton.Name = "exitButton";
+            this.exitButton.Size = new System.Drawing.Size(75, 23);
+            this.exitButton.TabIndex = 3;
+            this.exitButton.Text = "Exit";
+            this.exitButton.UseVisualStyleBackColor = true;
+            this.exitButton.Click += new System.EventHandler(this.exitButton_Click);
+            // 
+            // getPage
+            // 
+            this.getPage.Location = new System.Drawing.Point(367, 3);
+            this.getPage.Name = "getPage";
+            this.getPage.Size = new System.Drawing.Size(75, 23);
+            this.getPage.TabIndex = 2;
+            this.getPage.Text = "Get Page";
+            this.getPage.UseVisualStyleBackColor = true;
+            this.getPage.Click += new System.EventHandler(this.getPage_Click);
+            // 
+            // urlInput
+            // 
+            this.urlInput.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.urlInput.Location = new System.Drawing.Point(9, 3);
+            this.urlInput.Name = "urlInput";
+            this.urlInput.Size = new System.Drawing.Size(352, 20);
+            this.urlInput.TabIndex = 1;
+            this.urlInput.MouseClick += new System.Windows.Forms.MouseEventHandler(this.urlInput_MouseClick);
+            this.urlInput.TextChanged += new System.EventHandler(this.urlInput_TextChanged);
+            this.urlInput.Enter += new System.EventHandler(this.urlInput_Enter);
+            // 
+            // Form1
+            // 
+            this.AcceptButton = this.getPage;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.CancelButton = this.exitButton;
+            this.ClientSize = new System.Drawing.Size(532, 505);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.Name = "Form1";
+            this.Text = "HTML Grabber";
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.tableLayoutPanel1.PerformLayout();
+            this.flowLayoutPanel1.ResumeLayout(false);
+            this.flowLayoutPanel1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.RichTextBox dataOuput;
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel1;
+        private System.Windows.Forms.Button exitButton;
+        private System.Windows.Forms.Button getPage;
+        private System.Windows.Forms.TextBox urlInput;
+    }
+}
+
---
+++ HTMLGrabber/Form1.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.IO;
+using System.Net;
+using System.Text;
+using System.Windows.Forms;
+
+namespace SocketTest
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void flowLayoutPanel1_Resize(object sender, EventArgs e)
+        {
+
+        }
+
+        private void exitButton_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void getPage_Click(object sender, EventArgs e)
+        {
+            if (urlInput.Text != "")
+            {
+                dataOuput.Clear();
+                this.Text += " - " + urlInput.Text;
+                //dataOuput.AppendText("Getting URL: " + urlInput.Text + "\n");
+                WebResponse dataResponse;
+                StreamReader readResponse;
+                string result, url;
+                if (!urlInput.Text.Contains("http://"))
+                {
+                    url = "http://" + urlInput.Text;
+                }
+                else
+                {
+                    url = urlInput.Text;
+                }
+                try
+                {
+                    HttpWebRequest getData = (HttpWebRequest)WebRequest.Create(url);
+                    getData.Method = "GET";
+                    //getData.UserAgent = "Mozilla/5.0 (Windows; U; Windows NT 6.1; en-US; rv:1.9.2.8) Gecko/20100722 Firefox/3.6.8 GTB7.1";
+                    getData.UserAgent = "Mozilla/5.0 (Windows; U; Windows NT 6.0; en-US) AppleWebKit/532.0 (KHTML, like Gecko) Chrome/3.0.195.27 Safari/532.0 EVE-IGB";
+                    dataResponse = getData.GetResponse();
+                    readResponse = new StreamReader(dataResponse.GetResponseStream(), Encoding.UTF8);
+                    result = readResponse.ReadToEnd();
+                    dataOuput.AppendText(result);
+                    if (readResponse != null)
+                    {
+                        readResponse.Close();
+                    }
+                    if (dataResponse != null)
+                    {
+                        dataResponse.Close();
+                    }
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message);
+                    //throw;
+                }
+            }
+            else
+            {
+                MessageBox.Show("Please enter a URL in the box to the left.");
+            }
+        }
+
+        private void urlInput_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void urlInput_Enter(object sender, EventArgs e)
+        {
+            urlInput.SelectAll();
+        }
+
+        private void urlInput_MouseClick(object sender, MouseEventArgs e)
+        {
+            urlInput.SelectAll();
+        }
+    }
+}
